39f2f6d4a21dbe3fa43c24de92109013f1d9ab5f
Preeti Singh <115339754+ghpreeti@users.noreply.github.com>
2023-10-05 10:22:00 +0530

    Add files via upload

---
+++ lec2CipherSchool.cpp
@@ -0,0 +1,11 @@
+// #include<iostream> this is header joienoienv
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World"; 
+    //  this will print out Hello World on the user screen
+
+   
+return 0;
+}
